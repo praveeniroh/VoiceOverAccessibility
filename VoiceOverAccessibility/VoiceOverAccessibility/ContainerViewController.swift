f5b409c3dbeaa95d622396a2d92f636b6ed34f4c
praveen_iroh <prav7598@gmail.com>
2025-03-20 07:29:24 +0530

    Main story board removed

--- VoiceOverAccessibility/VoiceOverAccessibility/ContainerViewController.swift
+++ VoiceOverAccessibility/VoiceOverAccessibility/ContainerViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ContainerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemMint
     }
 
 
