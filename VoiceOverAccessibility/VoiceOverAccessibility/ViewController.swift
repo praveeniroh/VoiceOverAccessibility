6bfb0f1b5e162b5dc487d589f14e1cf2158cddcb
praveenraj t <praveenraj.ta@zohocorp.com>
2025-03-20 07:03:31 +0530

    New Xcode project added

---
+++ VoiceOverAccessibility/VoiceOverAccessibility/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoiceOverAccessibility
+//
+//  Created by praveen-12298 on 20/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
